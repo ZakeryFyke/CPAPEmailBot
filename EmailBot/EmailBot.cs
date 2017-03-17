e6583be22d931c167ced30ec3b23af21cfc4c5c3
Zak7062 <zak7062@gmail.com>
2017-03-16 22:35:44 -0500

    Pre-email additions commit

--- EmailBot/EmailBot.cs
+++ EmailBot/EmailBot.cs
@@ -33,35 +33,17 @@ namespace EmailBot
 
             var toEmailList = GetToEmailList(workbook);
 
+
+
         }
 
 
         static List<string> GetToEmailList(List<string> workbook)
         {
-            var toEmailList = new List<string>();
-
             // I really hope there's a better way to do something like this besides repeating it
-            var test = workbook.Where(x => (DateTime.Now - DateTime.Parse(x.Split(',').ToList()[2])).Days == 0 || (DateTime.Now - DateTime.Parse(x.Split(',').ToList()[2])).Days == 60)
+            var toEmailList = workbook.Where(x => (DateTime.Now - DateTime.Parse(x.Split(',').ToList()[2])).Days == 0 || (DateTime.Now - DateTime.Parse(x.Split(',').ToList()[2])).Days == 60)
                                .Select(x => x).ToList();
-
-            //.Where(y => (DateTime.Now - DateTime.Parse(y[2])).Days == 0 || (DateTime.Now - DateTime.Parse(y[3])).Days == 60).Select(y => y).ToList()
-
-
-            foreach (var line in workbook)
-            {
-                var lineList = line.Split(',').ToList();
-
-                var date = lineList[2]; // Probably better way to do this than hard coding
-
-                var timeDifference = DateTime.Now - DateTime.Parse(date);
-
-                if (timeDifference.Days == 0 || timeDifference.Days == 60) 
-                {
-                    toEmailList.Add(line);
-                }
-              
-            }
-
+            
             return toEmailList;
 
         }
