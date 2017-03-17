1f81892c9e93410cd71ccab26edb7da301b187c3
Zak7062 <zak7062@gmail.com>
2017-03-16 22:34:25 -0500

    Better linq way to get email list

--- EmailBot/EmailBot.cs
+++ EmailBot/EmailBot.cs
@@ -40,7 +40,14 @@ namespace EmailBot
         {
             var toEmailList = new List<string>();
 
-            foreach(var line in workbook)
+            // I really hope there's a better way to do something like this besides repeating it
+            var test = workbook.Where(x => (DateTime.Now - DateTime.Parse(x.Split(',').ToList()[2])).Days == 0 || (DateTime.Now - DateTime.Parse(x.Split(',').ToList()[2])).Days == 60)
+                               .Select(x => x).ToList();
+
+            //.Where(y => (DateTime.Now - DateTime.Parse(y[2])).Days == 0 || (DateTime.Now - DateTime.Parse(y[3])).Days == 60).Select(y => y).ToList()
+
+
+            foreach (var line in workbook)
             {
                 var lineList = line.Split(',').ToList();
 
@@ -48,11 +55,9 @@ namespace EmailBot
 
                 var timeDifference = DateTime.Now - DateTime.Parse(date);
 
-                var derf = timeDifference.Days;
-
                 if (timeDifference.Days == 0 || timeDifference.Days == 60) 
                 {
-                    Console.WriteLine(line);
+                    toEmailList.Add(line);
                 }
               
             }
