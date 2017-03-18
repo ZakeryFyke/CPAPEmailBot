4cc66764228939667a9af642ae9038681e9c3fd2
Zak7062 <zak7062@gmail.com>
2017-03-18 12:49:12 -0500

    Intermediate changes
    Probably need a more change tolerant way to grab items from files than
    just hard coding them, but that's for the future.

--- EmailBot/EmailBot.cs
+++ EmailBot/EmailBot.cs
@@ -2,6 +2,8 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Net;
+using System.Net.Mail;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -11,29 +13,61 @@ namespace EmailBot
     {
         static void Main(string[] args)
         {
+            var filePath = GetDocumentFilePath("EmailList.csv");
 
-            var filePath = AppDomain.CurrentDomain.BaseDirectory;
-
-            while (filePath != null && filePath != "")
+            if (filePath.Length == 0 || filePath == null)
             {
-                if(File.Exists(filePath + @"\EmailList.csv"))
-                {
-                    filePath = filePath + @"\EmailList.csv";
-                    break;
-                }
-                filePath = Directory.GetParent(filePath)?.FullName;
-            }
-
-            if (!filePath.Contains("EmailList.csv"))
-            {
-                Console.WriteLine("EmailList.csv not found.");
+                Console.WriteLine("EmailList.csv not found! Call Zak :)");
+                return;
             }
 
             var workbook = ReadWorkbook(filePath);
 
             var toEmailList = GetToEmailList(workbook);
 
+            SendEmails(toEmailList);
 
+        }
+
+
+        static void SendEmails(List<string> EmailData)
+        {
+            var filePath = GetDocumentFilePath("config.txt");
+
+            if(filePath == null || filePath.Length == 0)
+            {
+                Console.WriteLine("config.txt not found! Call Zak :)");
+                return;
+            }
+
+            var configData = new StreamReader(filePath);
+
+            var email = configData.ReadLine();
+            var password = configData.ReadLine();
+
+            var fromAddress = new MailAddress(email, "noreply@gmail.com");
+            var toAddress = new MailAddress(, "Steven Fry");
+            const string fromPassword = "y9-+ncAtVE=!!=*tiv+X";
+            const string subject = "Subject";
+            const string body = "Body";
+
+            var smtp = new SmtpClient
+            {
+                Host = "smtp.gmail.com",
+                Port = 587,
+                EnableSsl = true,
+                DeliveryMethod = SmtpDeliveryMethod.Network,
+                UseDefaultCredentials = false,
+                Credentials = new NetworkCredential(fromAddress.Address, fromPassword)
+            };
+            using (var message = new MailMessage(fromAddress, toAddress)
+            {
+                Subject = subject,
+                Body = body
+            })
+            {
+                smtp.Send(message);
+            }
 
         }
 
@@ -47,6 +81,24 @@ namespace EmailBot
             return toEmailList;
 
         }
+
+        static string GetDocumentFilePath(string fileName)
+        {
+            var filePath = AppDomain.CurrentDomain.BaseDirectory;
+
+            while (filePath != null && filePath != "")
+            {
+                if (File.Exists(filePath + @"\" + fileName))
+                {
+                    filePath = filePath + @"\" + fileName;
+                    break;
+                }
+                filePath = Directory.GetParent(filePath)?.FullName;
+            }
+
+            return filePath;
+        }
+
 
         static List<string> ReadWorkbook(string filePath)
         {
